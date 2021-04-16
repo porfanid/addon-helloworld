1ef91d83124b1f78b2841fc451998ef125b5f98d
Pavlos Orfanidis <pavlos@orfanidis.xyz>
2021-04-16 12:20:12 +0300

    finsished addon. translating it to python to see if it goes faster

--- server.js
+++ server.js
@@ -1,5 +1,4 @@
 const { serveHTTP } = require("stremio-addon-sdk");
 
 const addonInterface = require("./addon");
-serveHTTP(addonInterface, { port: 7000 });
-
+serveHTTP(addonInterface, { port: 3030 });
