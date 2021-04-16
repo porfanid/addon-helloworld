b8060c1f47de4022b5b7088ac1467082d1361931
Pavlos Orfanidis <pavlos@orfanidis.xyz>
2021-04-16 23:04:54 +0300

    updated the program to work faster
    splitted the program into 2 parts. The one will serve the files and the other will search for new movies

--- addon.js
+++ addon.js
@@ -1,13 +1,13 @@
 const { addonBuilder } = require("stremio-addon-sdk");
-const kickass = require("./kickass");
+const magnet = require("magnet-uri");
+const fs = require("fs");
 
 const manifest = {
-    "id": "org.stremio.helloworld",
+    "id": "org.zircon.stremio_addon",
     "version": "1.0.0",
 
-    "name": "Hello World Addon",
-    "description": "Sample addon providing a few public domain movies",
-    "logo": "https://www.stremio.com/website/stremio-logo-small.png",
+    "name": "All movies provided by Zircon",
+    "description": "Sample addon providing a lot of movies.",
 
     // set what type of resources we will return
     "resources": [
@@ -15,18 +15,92 @@ const manifest = {
         "stream"
     ],
 
-    "types": ["movie"], // your add-on will be preferred for those content types
+    "types": ["movie", "series"], // your add-on will be preferred for those content types
 
     // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
     "catalogs": [{
-        type: 'movie',
-        id: 'zircon-movies'
-    }],
+            type: 'movie',
+            id: 'helloworldmovies'
+        },
+        {
+            type: 'series',
+            id: 'helloworldseries'
+        }
+    ],
 
     // prefix of item IDs (ie: "tt0032138")
     "idPrefixes": ["tt"]
 
 };
+
+const dataset = {};
+// "tt0063350": fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce"),
+fs.readFile('./movies', 'utf8', function(err, data) {
+    if (err) {
+        return console.log(err);
+    }
+    data.split("\n").forEach((movie) => {
+        if (movie.match(/[^ ]+/g)) {
+            movie = movie.split(";");
+            id = movie[0];
+            title = movie[1];
+            type = movie[2];
+            magnet_link = movie[3];
+            dataset[id] = fromMagnet(title, type, magnet_link);
+        }
+    });
+});
+
+
+fs.watchFile("./movies", () => {
+    console.log("The file has been updated");
+    fs.readFile('./movies', 'utf8', function(err, data) {
+        if (err) {
+            return console.log(err);
+        }
+        data.split("\n").forEach((movie) => {
+            if (movie.match(/[^ ]+/g)) {
+                console.log(movie);
+                movie = movie.split(";");
+                id = movie[0];
+                title = movie[1];
+                type = movie[2];
+                magnet_link = movie[3];
+                dataset[id] = fromMagnet(title, type, magnet_link);
+            }
+        });
+    });
+});
+
+
+
+// utility function to add from magnet
+function fromMagnet(name, type, uri) {
+    const parsed = magnet.decode(uri);
+    const infoHash = parsed.infoHash.toLowerCase();
+    const tags = [];
+    if (uri.match(/720p/i)) tags.push("720p");
+    if (uri.match(/1080p/i)) tags.push("1080p");
+    return {
+        name: name,
+        type: type,
+        infoHash: infoHash,
+        sources: (parsed.announce || []).map(function(x) { return "tracker:" + x }).concat(["dht:" + infoHash]),
+        tag: tags,
+        title: tags[0], // show quality in the UI
+    }
+}
+
+const builder = new addonBuilder(manifest);
+
+// Streams handler
+builder.defineStreamHandler(function(args) {
+    if (dataset[args.id]) {
+        return Promise.resolve({ streams: [dataset[args.id]] });
+    } else {
+        return Promise.resolve({ streams: [] });
+    }
+})
 
 const METAHUB_URL = "https://images.metahub.space"
 
@@ -35,7 +109,6 @@ const generateMetaPreview = function(value, key) {
     // we'll fetch the poster from Stremio's MetaHub
     // see https://github.com/Stremio/stremio-addon-sdk/blob/master/docs/api/responses/meta.md#meta-preview-object
     const imdbId = key.split(":")[0]
-    console.log(METAHUB_URL + "/poster/medium/" + imdbId + "/img");
     return {
         id: imdbId,
         type: value.type,
@@ -44,47 +117,13 @@ const generateMetaPreview = function(value, key) {
     }
 }
 
-
-
-//----------------------------------------------------------------
-
-
-
-
-//let dataset = {};
-//kickass.then((data) => { dataset = data })
-
-const builder = new addonBuilder(manifest);
-
-// Streams handler
-builder.defineStreamHandler(function(args) {
-    console.log("starting");
-    return new Promise(function(resolve, reject) {
-        kickass.then((dataset) => {
-            if (dataset[args.id]) {
-                console.log("success");
-                resolve({ streams: [dataset[args.id]] })
-            } else {
-                console.log("something went wrong");
-                resolve({ streams: [] });
-            }
-        })
-    })
-});
-
-
 builder.defineCatalogHandler(function(args, cb) {
-    return new Promise(function(resolve, reject) {
-        kickass.then((dataset) => {
-            const metas = Object.entries(dataset)
-                .filter(([_, value]) => value.type === args.type)
-                .map(([key, value]) => generateMetaPreview(value, key));
+    // filter the dataset object and only take the requested type
+    const metas = Object.entries(dataset)
+        .filter(([_, value]) => value.type === args.type)
+        .map(([key, value]) => generateMetaPreview(value, key))
 
-
-            resolve({ metas: metas })
-        }).catch(reject)
-
-    });
+    return Promise.resolve({ metas: metas })
 })
 
 module.exports = builder.getInterface()
