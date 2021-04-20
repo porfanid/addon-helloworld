ed3da8dde28c64fd923040218a20c4e5b2878e44
Pavlos Orfanidis <pavlos@orfanidis.xyz>
2021-04-20 19:33:31 +0300

    fixed a minor bug

--- addon.js
+++ addon.js
@@ -1,9 +1,6 @@
 /*
 Import the packages to report the errors
 */
-
-let dataset = {};
-
 const Sentry = require("@sentry/node");
 // or use es6 import statements
 // import * as Sentry from '@sentry/node';
@@ -11,9 +8,7 @@ const Sentry = require("@sentry/node");
 const Tracing = require("@sentry/tracing");
 // or use es6 import statements
 // import * as Tracing from '@sentry/tracing';
-const { addonBuilder } = require("stremio-addon-sdk");
-const magnet = require("magnet-uri");
-const fs = require("fs");
+
 
 Sentry.init({
     dsn: "https://0c10594218a14d21811eac0317e9a76b@o238115.ingest.sentry.io/5727483",
@@ -23,45 +18,44 @@ Sentry.init({
     // We recommend adjusting this value in production
     tracesSampleRate: 1.0,
 });
+const { addonBuilder } = require("stremio-addon-sdk");
+const magnet = require("magnet-uri");
+const fs = require("fs");
 
-
-
-const read_file = function(dataset) {
-    fs.readFile('./movies', 'utf8', function(err, data) {
-        console.log("read the data.")
-        if (err) {
-            Sentry.captureException(err);
-            return console.log(err);
-        }
-        data.split("\n").forEach((movie) => {
-            if (movie.match(/[^ ]+/g)) {
-                movie = movie.split(";");
-                id = movie[0];
-                if (!(id in dataset)) {
-                    dataset[id] = [];
-                }
-                title = movie[1];
-                type = movie[2];
-                link = movie[3];
-                if (link == undefined) {
-                    console.log(movie);
-                    return;
-                }
-                if (link.startsWith("magnet")) {
-                    dataset[id].push(fromMagnet(title, type, link));
-                } else if (link.endsWith("mp4")) {
-                    dataset[id].push({ name: title, type: type, url: link });
-                } else if (link.indexOf("youtube") > -1) {
-                    yt_id = link.split("v=")[1].split("&")[0];
-                    dataset[id].push({ name: title, type: type, ytId: yt_id });
-                }
+const read_file = function() {
+    const dataset = {};
+    const data = fs.readFileSync("./movies", "utf8", Sentry.captureException);
+    data.split("\n").forEach((movie) => {
+        if (movie.match(/[^ ]+/g)) {
+            movie = movie.split(";");
+            id = movie[0];
+            if (!(id in dataset)) {
+                dataset[id] = [];
             }
-        });
+            title = movie[1];
+            type = movie[2];
+            link = movie[3];
+            if (link == undefined) {
+                console.log(movie);
+                return;
+            }
+
+            if (link.startsWith("magnet")) {
+                console.log(link);
+                dataset[id].push(fromMagnet(title, type, link));
+            } else if (link.endsWith("mp4")) {
+                dataset[id].push({ name: title, type: type, url: link });
+            } else if (link.indexOf("youtube") > -1) {
+                yt_id = link.split("v=")[1].split("&")[0];
+                dataset[id].push({ name: title, type: type, ytId: yt_id });
+            }
+        }
     });
+    return dataset;
 }
 
 const manifest = {
-    "id": "org.zircon.stremio_addon",
+    "id": "community.stremio_addon",
     "version": "1.0.0",
 
     "name": "All movies provided by Zircon",
@@ -77,23 +71,29 @@ const manifest = {
 
     // set catalogs, we'll be making 2 catalogs in this case, 1 for movies and 1 for series
     "catalogs": [{
-            type: 'movie',
-            id: 'helloworldmovies'
-        },
-        {
-            type: 'series',
-            id: 'helloworldseries'
-        }
-    ],
-
-    // prefix of item IDs (ie: "tt0032138")
-    "idPrefixes": ["tt"]
+        type: 'movie',
+        id: 'movie'
+    }]
 
 };
-read_file(dataset);
-fs.watchFile("./movies", () => read_file(dataset));
 
-setTimeout(() => { console.log(dataset) }, 3000);
+const dataset = {
+    // fileIdx is the index of the file within the torrent ; if not passed, the largest file will be selected
+    "tt0032138": [{ name: "The Wizard of Oz", type: "movie", infoHash: "24c8802e2624e17d46cd555f364debd949f2c81e", fileIdx: 0 }],
+    "tt0017136": [{ name: "Metropolis", type: "movie", infoHash: "dca926c0328bb54d209d82dc8a2f391617b47d7a", fileIdx: 1 }],
+
+    // night of the living dead, example from magnet
+    "tt0063350": [fromMagnet("Night of the Living Dead", "movie", "magnet:?xt=urn:btih:A7CFBB7840A8B67FD735AC73A373302D14A7CDC9&dn=night+of+the+living+dead+1968+remastered+bdrip+1080p+ita+eng+x265+nahom&tr=udp%3A%2F%2Ftracker.publicbt.com%2Fannounce&tr=udp%3A%2F%2Fglotorrents.pw%3A6969%2Fannounce&tr=udp%3A%2F%2Ftracker.openbittorrent.com%3A80%2Fannounce")],
+    "tt0051744": [{ name: "House on Haunted Hill", type: "movie", infoHash: "9f86563ce2ed86bbfedd5d3e9f4e55aedd660960" }],
+
+    "tt1254207": [{ name: "Big Buck Bunny", type: "movie", url: "http://clips.vorwaerts-gmbh.de/big_buck_bunny.mp4" }], // HTTP stream
+    "tt0031051": [{ name: "The Arizona Kid", type: "movie", ytId: "m3BKVSpP80s" }], // YouTube stream
+
+    "tt0137523": [{ name: "Fight Club", type: "movie", externalUrl: "https://www.netflix.com/watch/26004747" }], // redirects to Netflix
+
+    "tt1748166:1:1": [{ name: "Pioneer One", type: "series", infoHash: "07a9de9750158471c3302e4e95edb1107f980fa6" }], // torrent for season 1, episode 1
+};
+// console.log(dataset);
 
 // utility function to add from magnet
 function fromMagnet(name, type, uri) {
@@ -103,12 +103,12 @@ function fromMagnet(name, type, uri) {
     if (uri.match(/720p/i)) tags.push("720p");
     if (uri.match(/1080p/i)) tags.push("1080p");
     return {
-        name: name,
+        name: tags[0],
         type: type,
         infoHash: infoHash,
         sources: (parsed.announce || []).map(function(x) { return "tracker:" + x }).concat(["dht:" + infoHash]),
         tag: tags,
-        title: tags[0], // show quality in the UI
+        title: name, // show quality in the UI
     }
 }
 
@@ -117,8 +117,10 @@ const builder = new addonBuilder(manifest);
 // Streams handler
 builder.defineStreamHandler(function(args) {
     if (dataset[args.id]) {
+        console.log("success");
         return Promise.resolve({ streams: dataset[args.id] });
     } else {
+        console.log("fail");
         return Promise.resolve({ streams: [] });
     }
 })
